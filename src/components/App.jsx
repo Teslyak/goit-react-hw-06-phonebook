d5c4511c90483095cb3e3a85d802010a8279387b
Teslyak <124871929+Teslyak@users.noreply.github.com>
2023-09-10 09:55:02 +0300

    hw4-react-phonebook-fix-app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,39 +1,34 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { AddContact } from './AddContact/AddContact'
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { DivWrap } from './App.styled';
 
-
-export class App extends Component {
-  state = {
-    contacts: [
+const initContacts = [
     {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
     {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
     {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
     {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
-    filter: ''
-  }
-  componentDidMount() {
-    const contactList = localStorage.getItem('contact-list')
-    if (contactList !== null) {
-      this.setState({
-       contacts: JSON.parse(contactList) 
-      })
-    };
-   
- }
+    ]
+export const App = () => {
+  const [contacts, setContacts] = useState(initContacts)
+  const [filter, setFilter] = useState('')
 
-  componentDidUpdate(pState) {
-    if (pState.contacts !== this.state.contacts) {
-      localStorage.setItem('contact-list', JSON.stringify(this.state.contacts) )
-    }
-      
-  }
+useEffect(() => {
+   const contactList = localStorage.getItem('contact-list')
+  if (contactList !== null) {
+      setContacts(JSON.parse(contactList))
+      }
+}, [])
+
+useEffect(() => {
+ 
+localStorage.setItem('contact-list', JSON.stringify(contacts) )
   
-  addContact = contact => {
-    const chekContact = this.state.contacts.some(el => {
+}, [contacts])
+  
+  const addContact = contact => {
+    const chekContact = contacts.some(el => {
      return el.name.trim() === contact.name.trim()  
     })
     
@@ -42,42 +37,37 @@ export class App extends Component {
       return
     }
 
-    this.setState((prevState) => ({
-      contacts: [...prevState.contacts, contact]
-    }))
+    setContacts( [...contacts, contact])
   }
 
-  handleFilter =  (evt) => {
-    this.setState({ filter: evt.target.value });
+ const  handleFilter =  (evt) => {
+    setFilter(evt.target.value) 
 
   }
 
-  getFilterText = () => {
-    const normalizedFilter = this.state.filter.toLowerCase().trim();
-    return this.state.contacts.filter((el) => {
+  const getFilterText = () => {
+    const normalizedFilter = filter.toLowerCase().trim();
+    return contacts.filter((el) => {
       return el.name.toLowerCase().includes(normalizedFilter);
     });
   }
-  handleDelete = id => {
-    this.setState(pState => ({
-    contacts: pState.contacts.filter(el =>el.id !== id)
-  }) )
+
+  const handleDelete = id => {
+   setContacts(contacts.filter(el =>el.id !== id)) 
+   
 }
 
-
-  render() {
    
     return (
       <DivWrap>
         <h1>Phonebook</h1>
-        <AddContact addNewContact={this.addContact} /> 
+        <AddContact addNewContact={addContact} /> 
         <h2>Contacts</h2>
-        <Filter handleFilter={this.handleFilter} />
-        <ContactList onDeleteContact={this.handleDelete} getFilterText={this.getFilterText()} />
+        <Filter handleFilter={handleFilter} />
+        <ContactList onDeleteContact={handleDelete} getFilterText={getFilterText()} />
       </DivWrap>
       
   )}
   
-}
 
 
